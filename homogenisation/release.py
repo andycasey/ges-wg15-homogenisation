49dd72dd4c6440b3f7620b0d5e90130f5f647524
Andy Casey <andycasey@gmail.com>
2015-01-20 19:27:47 +0000

    ingest results from WGs

--- homogenisation/release.py
+++ homogenisation/release.py
@@ -9,26 +9,50 @@ __author__ = "Andy Casey <arc@ast.cam.ac.uk>"
 # Standard library
 import logging
 
+# Module-specific
+from wg import WorkingGroupResults
+
 # Create a logger.
 logger = logging.getLogger(__name__)
 
 
 class DataRelease(object):
 
-    def __init__(self, version=None):
-
+    def __init__(self, version):
+        logger.debug("Created DataRelease object with version {}".format(version))
         self.version = version
 
 
-
     def ingest(self, filenames, validate=True):
+        """
+        Ingest recommended results files from the Working Group leads.
+
+        :param filenames:
+            A list of filenames containing results from the Working Group leads.
+
+        :type filenames:
+            list of str
+
+        :param validate: [optional]
+            Validate that the input files do not contain any unexpected data.
+
+        :type validate:
+            bool
+        """
+
+        if not isinstance(filenames, (tuple, list)):
+            raise TypeError("expected list of filenames for DataRelease object")
 
         # Load the working group files into this DataRelease
+        self._wg_results = \
+            [WorkingGroupResults.from_filename(filename, validate=validate) \
+                for filename in filenames]
 
-        raise NotImplementedError
-
-
-
+        # Keep an attribute for accessing which WG they are.
+        self._wg_names = [_.wg for _ in self._wg_results]
+        return True
+    
+    
     def update_results(self, rule):
         raise NotImplementedError
 
